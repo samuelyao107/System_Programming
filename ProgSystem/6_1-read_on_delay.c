6415b62cfe1064f65f6c364eb412dd217fb507c9
samuelyao107 <samuelyao107@gmail.com>
2026-03-04 23:04:41 +0100

    exercice 6

--- ProgSystem/6_1-read_on_delay.c
+++ ProgSystem/6_1-read_on_delay.c
@@ -18,17 +18,61 @@
  * 
  * Sous Solaris on utilisera truss au lieu de strace
 */
-
+#define _POSIX_C_SOURCE 200809L 
+#include <signal.h>
 #include <sys/fcntl.h>
 #include <unistd.h>
 #include <stdlib.h>
 #include <string.h>
 #include <stdio.h>
 
+void hdl_block(int n){
+  int flag = fcntl(STDIN_FILENO, F_GETFL, 0);
+  if(flag < 0) {
+    perror("Fcntl F_GETFL");
+    exit(EXIT_FAILURE);
+  }
+
+  flag = flag & ~O_NONBLOCK;
+  if(fcntl(STDIN_FILENO, F_SETFL, flag) < 0) {
+    perror("Fcntl F_SETFL");
+    exit(EXIT_FAILURE);
+  }
+
+  printf("Mode BLOQUANT\n", n);
+}
+
+void hdl_non_block(int n){
+  int flag = fcntl(STDIN_FILENO, F_GETFL, 0);
+  if(flag < 0) {
+    perror("Could not get flags");
+    exit(EXIT_FAILURE);
+  }
+
+  flag = flag | O_NONBLOCK;
+  if(fcntl(STDIN_FILENO, F_SETFL, flag) < 0) {
+    perror("Could not set flags");
+    exit(EXIT_FAILURE);
+  }
+
+  printf("Mode NON-BLOQUANT\n", n);
+}
+
+
 
 int main(int argc, char **argv) {
   int flag, r;
   char buf[10];
+
+  struct sigaction sa;
+  sigemptyset(&sa.sa_mask);
+  sa.sa_flags = 0;
+
+  sa.sa_handler = hdl_block;
+  sigaction(SIGINT, &sa, NULL);
+
+  sa.sa_handler = hdl_non_block;
+  sigaction(SIGQUIT, &sa, NULL);
 
   if ((argc > 1) && (strcmp(argv[1], "true") == 0)) {
     flag = fcntl(STDIN_FILENO, F_GETFL, 0);
@@ -37,7 +81,7 @@ int main(int argc, char **argv) {
       exit(EXIT_FAILURE);
     }
     /* positionner maintenant le flag avec O_NONBLOCK */
-    flag = fcntl(STDIN_FILENO, ? ? ? ? ? ? ? ? ? ? ? ? ? ? ? ? ?);
+    flag = fcntl(STDIN_FILENO, F_SETFL, flag | O_NONBLOCK);
 
     if (flag < 0) {
       perror("Fcntl F_SETFL");
