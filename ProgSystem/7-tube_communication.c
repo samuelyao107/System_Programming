8bbbcfd285a4073e757f5c01a4e654f042cc7bd4
samuelyao107 <samuelyao107@gmail.com>
2026-03-06 10:36:12 +0100

    the end

--- ProgSystem/7-tube_communication.c
+++ ProgSystem/7-tube_communication.c
@@ -11,62 +11,34 @@
 #include <sys/wait.h>
 
 int main() {
-    int fd[2];      // fd[0] = lecture, fd[1] = écriture
-    pid_t pid;
-    char buf[256];
+    int com[2];
+    char buff[256];
     int n;
+    pipe(com);
+    pid_t pid = fork();
+    if (pid == 0){
+        /*Inside the son*/
 
-    /*Création du tube */
-    if (pipe(fd) < 0) {
-        perror("Erreur lors de la création du tube");
-        exit(EXIT_FAILURE);
-    }
-
-    /*Création du fils */
-    pid = fork();
-    if (pid < 0) {
-        perror("Erreur lors de la création du fils");
-        exit(EXIT_FAILURE);
-    }
-
-    /* -------------------------------------------------- */
-    /*                      FILS                          */
-    /* -------------------------------------------------- */
-    if (pid == 0) {
-
-        /* Rediriger stdin → fd[0] (lecture du tube) */
-        dup2(fd[0], STDIN_FILENO);
-
-        /* Fermer les descripteurs inutiles */
-        close(fd[0]);
-        close(fd[1]);
-
-        /* Lire stdin (= le tube) et afficher */
-        while ((n = read(STDIN_FILENO, buf, sizeof(buf))) > 0) {
-            write(STDOUT_FILENO, "[FILS reçoit] : ", 16);
-            write(STDOUT_FILENO, buf, n);
+      close(com[1]);
+      dup2(com[0], STDIN_FILENO);
+        while ((n = read(STDIN_FILENO, buff, sizeof(buff))) > 0) {
+            buff[n] = '\0';
+            printf("Fils reçu : %s", buff); 
+            fflush(stdout);
         }
-
-        exit(EXIT_SUCCESS);
     }
-
-    /* -------------------------------------------------- */
-    /*                      PÈRE                          */
-    /* -------------------------------------------------- */
-
-    /* Rediriger stdout → fd[1] (écriture dans le tube) */
-    dup2(fd[1], STDOUT_FILENO);
-
-    /* Fermer les descripteurs inutiles */
-    close(fd[0]);
-    close(fd[1]);
-
-    /* Lire le clavier et écrire dans stdout (= le tube) */
-    while ((n = read(STDIN_FILENO, buf, sizeof(buf))) > 0) {
-        write(STDOUT_FILENO, buf, n);   // stdout = tube → le fils reçoit
+    else if(pid>0){
+        /*Inside the father*/
+       close(com[0]);
+       dup2(com[1], STDOUT_FILENO);
+       while (1) {
+            fgets(buff, sizeof(buff), stdin);
+            write(com[1], buff, sizeof(buff));
+      }
+      close(STDOUT_FILENO); 
+      wait(NULL);
     }
-
-    /* Attendre la fin du fils */
-    wait(NULL);
-    exit(EXIT_SUCCESS);
+    else{
+        perror("fork failed");
+    }
 }
